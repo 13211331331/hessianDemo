35a629721136a9dfe203940ad80a2ca58a204e14
huanghanlin <huanghanlin@onedt.cn>
2014-10-16 18:10:57 +0800

    init

---
+++ client-webapp/src/test/java/com/hhl/HessianTest.java
@@ -0,0 +1,16 @@
+package com.hhl;
+
+import com.hhl.service.IHello;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by admin on 2014/10/16.
+ */
+public class HessianTest {
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("servlet.xml");  //这里只是你声明的bean的xml文件所在的路径
+        IHello b = (IHello) context.getBean("myServiceClient");
+        System.out.println(b.sayHello());
+    }
+}
